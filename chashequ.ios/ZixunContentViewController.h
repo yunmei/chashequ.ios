30ce0bbdd8c6d8b5f0a672de6652e3e3df297e36
wanghong <89240220@qq.com>
2013-03-07 13:39:08 +0800

    完善内容页

--- chashequ.ios/ZixunContentViewController.h
+++ chashequ.ios/ZixunContentViewController.h
@@ -8,7 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ZixunContentViewController : UIViewController
+@interface ZixunContentViewController : UIViewController<
+UIWebViewDelegate,
+UIScrollViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIScrollView *contentScrollView;
 @property (strong,nonatomic)UISwipeGestureRecognizer *swipeGesture;
 @property (strong,nonatomic)NSString *zixunId;
+@property (strong,nonatomic)UIWebView *contentWebView;
+@property (strong,nonatomic)UILabel *contentTitleLable;
+@property (strong,nonatomic)UIView *headerView;
+@property (strong,nonatomic)UILabel *detailLable;
 @end
--- chashequ.ios/ZixunContentViewController.m
+++ chashequ.ios/ZixunContentViewController.m
@@ -17,6 +17,10 @@
 @implementation ZixunContentViewController
 @synthesize swipeGesture = _swipeGesture;
 @synthesize zixunId;
+@synthesize contentWebView = _contentWebView;
+@synthesize contentTitleLable = _contentTitleLable;
+@synthesize detailLable;
+@synthesize contentScrollView;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -30,11 +34,63 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self.contentScrollView setFrame:CGRectMake(0, 0, 320, 415)];
     [self.view addGestureRecognizer:self.swipeGesture];
+    //生成底部返回和分享按钮
+    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [backButton setFrame:CGRectMake(0, 415, 160, 45)];
+    [backButton setBackgroundImage:[UIImage imageNamed:@"ContentBack.png"] forState:UIControlStateNormal];
+    [backButton addTarget:self action:@selector(goBack:) forControlEvents:UIControlEventTouchUpInside];
+    UIButton *shareButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [shareButton setFrame:CGRectMake(160, 415, 160, 45)];
+    [shareButton setBackgroundImage:[UIImage imageNamed:@"ContentShare.png"] forState:UIControlStateNormal];
+    [shareButton addTarget:self action:@selector(contentShare:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:shareButton];
+    [self.view addSubview:backButton];
+    //生成头部绿色背景
+    self.headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 120)];
+    [self.headerView setBackgroundColor:[UIColor colorWithRed:61.0/255.0 green:157.0/255.0 blue:1.0/255.0 alpha:1.0]];
+    [self.view addSubview:self.headerView];
     NSMutableDictionary *params = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"news.getNewsContent",@"method",self.zixunId,@"id", nil];
     MKNetworkOperation *op = [YMGlobal getOperation:params];
     [op addCompletionHandler:^(MKNetworkOperation *completedOperation) {
         NSLog(@"zixuncontent%@",[completedOperation responseString]);
+        SBJsonParser *parser = [[SBJsonParser alloc]init];
+        NSMutableDictionary *object = [parser objectWithData:[completedOperation responseData]];
+        if([[object objectForKey:@"errorMessage"]isEqualToString:@"success"])
+        {
+            NSLog(@"%@",object);
+            NSMutableDictionary *data = [object objectForKey:@"data"];
+            NSString *titleContent = [data objectForKey:@"title"];
+            NSString *content = [data objectForKey:@"content"];
+            if(titleContent)
+            {
+                //计算内容所需要的高度
+                CGSize size = [titleContent sizeWithFont:[UIFont systemFontOfSize:22.0] constrainedToSize:CGSizeMake(280.0, 1000.0f) lineBreakMode:UILineBreakModeWordWrap];
+                CGFloat height = size.height;
+                self.contentTitleLable = [[UILabel alloc]initWithFrame:CGRectMake(20, 10, 280, height+20)];
+                [self.contentTitleLable setNumberOfLines:0];
+                self.contentTitleLable.text = titleContent;
+                self.contentTitleLable.backgroundColor = [UIColor clearColor];
+                [self.contentTitleLable setFont:[UIFont systemFontOfSize:22.0]];
+                [self.contentTitleLable setTextColor:[UIColor whiteColor]];
+                [self.view addSubview:self.contentTitleLable];
+                //设置来源，作者，发表时间
+                self.detailLable = [[UILabel alloc]initWithFrame:CGRectMake(20, height+40, 280, 20)];
+                [self.detailLable setFont:[UIFont systemFontOfSize:14.0]];
+                [self.detailLable setText:[NSString stringWithFormat:@"%@   %@    %@",[data objectForKey:@"source"],[data objectForKey:@"author"],[data objectForKey:@"create_time"]]];
+                [self.detailLable setBackgroundColor:[UIColor clearColor]];
+                [self.detailLable setTextColor:[UIColor whiteColor]];
+                [self.view addSubview:self.detailLable];
+            }
+            if(content)
+            {
+                self.contentWebView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 120, 320, 295)];
+                //self.contentWebView
+                [self.view addSubview:self.contentWebView];
+                [self.contentWebView loadHTMLString:content baseURL:[NSURL URLWithString:@"about:blank"]];
+            }
+        }
     } errorHandler:^(MKNetworkOperation *completedOperation, NSError *error) {
         NSLog(@"%@",error);
     }];
@@ -55,5 +111,26 @@
     }
     _swipeGesture.direction = UISwipeGestureRecognizerDirectionRight;
     return _swipeGesture;
+}
+
+- (void)goBack:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)contentShare:(id)sender
+{
+    NSLog(@"shareContent");
+}
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+
+}
+
+
+- (void)viewDidUnload {
+    [self setContentScrollView:nil];
+    [super viewDidUnload];
 }
 @end
