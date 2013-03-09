865af5ac7a0653dcecc7f6988058c3020df7d8fe
wanghong <89240220@qq.com>
2013-03-09 14:37:29 +0800

    完成内容页的新浪微博分享

--- chashequ.ios/ZixunContentViewController.h
+++ chashequ.ios/ZixunContentViewController.h
@@ -7,10 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h>
+#import "SinaWeibo.h"
+#import "SinaWeiboRequest.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ZixunContentViewController : UIViewController<
 UIWebViewDelegate,
-UIScrollViewDelegate>
+SinaWeiboDelegate,
+SinaWeiboRequestDelegate
+>
 
 @property (strong, nonatomic) IBOutlet UIScrollView *contentScrollView;
 @property (strong,nonatomic)UISwipeGestureRecognizer *swipeGesture;
@@ -20,5 +26,25 @@ UIScrollViewDelegate>
 @property (strong,nonatomic)UIView *headerView;
 @property (strong,nonatomic)UILabel *detailLable;
 @property (strong,nonatomic)NSString *shareContent;
-@property (strong,nonatomic) UIView *shareView;
+@property (strong,nonatomic) UIImageView *shareViewBar;
+
+@property (strong, nonatomic) UITextView *textView;
+
+@property (strong, nonatomic) UIView *shareView;
+
+@property (strong, nonatomic) UIActivityIndicatorView *indicator;
+
+
+- (void) addButton;
+
+- (void) addShareView;
+
+- (void) share:(UIButton*) sender;
+
+- (void) removeShare:(UIButton*) sender;
+
+- (void) sendShare:(UIButton*) sender;
+
+- (void) exitShare:(UIButton*) sender;
+
 @end
--- chashequ.ios/ZixunContentViewController.m
+++ chashequ.ios/ZixunContentViewController.m
@@ -22,7 +22,11 @@
 @synthesize detailLable;
 @synthesize contentScrollView;
 @synthesize shareContent;
+@synthesize shareViewBar = _shareViewBar;
+@synthesize textView = _textView;
 @synthesize shareView = _shareView;
+@synthesize indicator = _indicator;
+BOOL ISSHARESHOW = NO;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -58,7 +62,7 @@
                 //计算内容所需要的高度
                 CGSize size = [titleContent sizeWithFont:[UIFont systemFontOfSize:22.0] constrainedToSize:CGSizeMake(280.0, 1000.0f) lineBreakMode:UILineBreakModeWordWrap];
                 CGFloat height = size.height;
-                self.contentTitleLable = [[UILabel alloc]initWithFrame:CGRectMake(20, 10, 280, height+20)];
+                self.contentTitleLable = [[UILabel alloc]initWithFrame:CGRectMake(10, 10, 280, height)];
                 [self.contentTitleLable setNumberOfLines:0];
                 self.contentTitleLable.text = titleContent;
                 self.contentTitleLable.backgroundColor = [UIColor clearColor];
@@ -66,22 +70,24 @@
                 [self.contentTitleLable setTextColor:[UIColor whiteColor]];
                 [self.headerView addSubview:self.contentTitleLable];
                 //设置来源，作者，发表时间
-                self.detailLable = [[UILabel alloc]initWithFrame:CGRectMake(20, height+40, 280, 20)];
+                self.detailLable = [[UILabel alloc]initWithFrame:CGRectMake(10, height+20, 280, 20)];
                 [self.detailLable setFont:[UIFont systemFontOfSize:14.0]];
                 [self.detailLable setText:[NSString stringWithFormat:@"%@   %@    %@",[data objectForKey:@"source"],[data objectForKey:@"author"],[data objectForKey:@"create_time"]]];
                 [self.detailLable setBackgroundColor:[UIColor clearColor]];
                 [self.detailLable setTextColor:[UIColor whiteColor]];
                 [self.headerView  addSubview:self.detailLable];
+                [self.headerView setFrame:CGRectMake(0,0,320, height+45)];
                 //设置分享内容
                 self.shareContent = [titleContent stringByAppendingString:weiboUrl];
+                if(content)
+                {
+                    self.contentWebView = [[UIWebView alloc]initWithFrame:CGRectMake(0, height+45, 320, 295)];
+                    self.contentWebView.delegate = self;
+                    [self.contentWebView loadHTMLString:content baseURL:[NSURL URLWithString:@"about:blank"]];
+                    
+                }
             }
-            if(content)
-            {
-                self.contentWebView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 120, 320, 295)];
-                self.contentWebView.delegate = self;
-                [self.contentWebView loadHTMLString:content baseURL:[NSURL URLWithString:@"about:blank"]];
-                
-            }
+
         }
     } errorHandler:^(MKNetworkOperation *completedOperation, NSError *error) {
         NSLog(@"%@",error);
@@ -123,15 +129,23 @@
 
 - (void)contentShare:(id)sender
 {
+    if(!ISSHARESHOW)
+    {
+        [self.view addSubview:self.shareViewBar];
+        ISSHARESHOW =YES;
+    }
+    else
+    {
+        [self.shareViewBar removeFromSuperview];
+        ISSHARESHOW = NO;
+    }
+        
 //    SNViewController *snView = [[SNViewController alloc]init];
 //    snView.weiboContent = self.shareContent;
 //    [self.navigationController pushViewController:snView animated:YES];
 }
 
--(void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
 
-}
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
@@ -148,30 +162,165 @@
     [super viewDidUnload];
 }
 
-- (UIView *)shareView
+- (UIImageView *)shareViewBar
 {
-    _shareView =[[UIView alloc]initWithFrame:CGRectMake(20, 360, 280, 40)];
+    if(_shareViewBar == nil)
+    _shareViewBar =[[UIImageView alloc]initWithFrame:CGRectMake(20, 345, 280 , 65)];
+    [_shareViewBar setImage:[UIImage imageNamed:@"ShareBar.png"]];
+    [_shareViewBar setUserInteractionEnabled:YES];
     UIButton *sinaShareButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [sinaShareButton setFrame:CGRectMake(10, 5, 120, 30)];
-    [sinaShareButton setTitle:@"新浪微博" forState:UIControlStateNormal];
-    [sinaShareButton addTarget:self action:@selector(sinaShare:) forControlEvents:UIControlStateNormal];
+    [sinaShareButton setFrame:CGRectMake(10, 12, 120, 30)];
+    [sinaShareButton setBackgroundImage:[UIImage imageNamed:@"SinaWeiboShare.png"] forState:UIControlStateNormal];
+    [sinaShareButton addTarget:self action:@selector(sinaShare:) forControlEvents:UIControlEventTouchUpInside];
     UIButton *tengxunButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [tengxunButton setFrame:CGRectMake(140, 5, 120, 30)];
-    [tengxunButton setTitle:@"腾讯微博" forState:UIControlStateNormal];
-    [tengxunButton addTarget:self action:@selector(tentxunShare:) forControlEvents:UIControlStateNormal];
+    [tengxunButton setFrame:CGRectMake(145, 12, 120, 30)];
+    [tengxunButton setBackgroundImage:[UIImage imageNamed:@"TengxunWeiboShare.png"] forState:UIControlStateNormal];
+    [tengxunButton addTarget:self action:@selector(tentxunShare:) forControlEvents:UIControlEventTouchUpInside];
     
-    [_shareView addSubview:sinaShareButton];
-    [_shareView addSubview:tengxunButton];
-    return _shareView;
+    [_shareViewBar addSubview:sinaShareButton];
+    [_shareViewBar addSubview:tengxunButton];
+    return _shareViewBar;
 }
 
+//点击新浪分享
 - (void)sinaShare:(id)sender
 {
+    SinaWeibo *sinaWeibo = [self sinaWeibo];
     
+    BOOL authValid = sinaWeibo.isAuthValid;
+    
+    if (!authValid)
+    {
+        [sinaWeibo logIn];
+    }
+    else
+    {
+        [self addShareView];
+        [_textView becomeFirstResponder];
+    }
 }
 
 - (void)tentxunShare:(id)sender
 {
     
 }
+
+//以下所有内容为新浪微博分享部分
+
+- (SinaWeibo*)sinaWeibo
+{
+    AppDelegate *delegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    delegate.sinaweibo.delegate = self;
+    return delegate.sinaweibo;
+}
+
+//登陆成功后回调
+- (void) sinaweiboDidLogIn:(SinaWeibo *)sinaweibo
+{
+    [self addShareView];
+    [_textView becomeFirstResponder];
+}
+
+//取消按钮回调方法
+- (void) removeShare:(UIButton*) sender
+{
+    [_shareView removeFromSuperview];
+}
+
+//发送按钮回调方法
+- (void) sendShare:(UIButton*) sender
+{
+    NSString *postStatusText = self.textView.text;
+    
+    SinaWeibo *sinaWeibo = [self sinaWeibo];
+    
+    [sinaWeibo requestWithURL:@"statuses/update.json" params:[NSMutableDictionary dictionaryWithObjectsAndKeys:postStatusText,@"status", nil] httpMethod:@"POST" delegate:self];
+    
+    [_shareView removeFromSuperview];
+    
+    [self.indicator startAnimating];
+}
+
+//退出登陆回调方法
+- (void) exitShare:(UIButton*) sender
+{
+    SinaWeibo *sinaWeibo = [self sinaWeibo];
+    
+    [sinaWeibo logOut];
+    
+    [_shareView removeFromSuperview];
+    
+    NSLog(@"退出登陆");
+}
+
+//请求完成回调该方法
+- (void)request:(SinaWeiboRequest *)request didFinishLoadingWithResult:(id)result
+{
+    [self.indicator stopAnimating];
+    
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"分享成功" message:@"提示" delegate:self cancelButtonTitle:@"确定" otherButtonTitles: nil];
+    [alert show];
+    NSLog(@"发送成功");
+    NSLog(@"result%@",result);
+}
+
+//请求失败回调该方法
+- (void)request:(SinaWeiboRequest *)request didFailWithError:(NSError *)error
+{
+    [self.indicator stopAnimating];
+    
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"发送失败,请检测网络链接" message:@"提示" delegate:self cancelButtonTitle:@"确定" otherButtonTitles: nil];
+    [alert show];
+    NSLog(@"发送失败");
+}
+
+- (void) addShareView
+{
+    _shareView = [[UIView alloc] initWithFrame:CGRectMake(20, 30, 280, 200)];
+    _shareView.layer.masksToBounds = YES;
+    _shareView.layer.cornerRadius = 6.0;
+    _shareView.layer.borderWidth = 1.0;
+    _shareView.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:_shareView];
+    
+    UIButton *quXiaoButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [quXiaoButton setFrame:CGRectMake(5, 5, 60, 30)];
+    [quXiaoButton setTitle:@"取消" forState:UIControlStateNormal];
+    [quXiaoButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [quXiaoButton addTarget:self action:@selector(removeShare:) forControlEvents:UIControlEventTouchUpInside];
+    [_shareView addSubview:quXiaoButton];
+    
+    UIButton *fenXiangButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [fenXiangButton setFrame:CGRectMake(280-65, 5, 60, 30)];
+    [fenXiangButton setTitle:@"发送" forState:UIControlStateNormal];
+    [fenXiangButton addTarget:self action:@selector(sendShare:) forControlEvents:UIControlEventTouchUpInside];
+    [fenXiangButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [_shareView addSubview:fenXiangButton];
+    
+    UIButton *tuiChuButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [tuiChuButton setFrame:CGRectMake(5, 165, 100, 30)];
+    [tuiChuButton setTitle:@"退出登陆" forState:UIControlStateNormal];
+    [tuiChuButton addTarget:self action:@selector(exitShare:) forControlEvents:UIControlEventTouchUpInside];
+    [tuiChuButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [_shareView addSubview:tuiChuButton];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(55, 5, 280-110, 30)];
+    label.textAlignment = UITextAlignmentCenter;
+    label.text = @"新浪微博";
+    label.textColor = [UIColor whiteColor];
+    label.backgroundColor = [UIColor clearColor];
+    label.font = [UIFont fontWithName:[[UIFont familyNames] objectAtIndex:0] size:20];
+    [_shareView addSubview:label];
+    
+    _textView = [[UITextView alloc] initWithFrame:CGRectMake(5, 40, 270, 120)];
+    _textView.layer.cornerRadius = 6.0;
+    [_textView becomeFirstResponder];
+    [_textView setTextAlignment:UITextAlignmentLeft];
+    [_textView setBackgroundColor:[UIColor whiteColor]];
+    [_textView becomeFirstResponder];
+    _textView.text = self.shareContent;
+    _textView.keyboardType = UIKeyboardTypeASCIICapable;
+    [_shareView addSubview:_textView];
+}
+
 @end
